5eabbdd7f17724c7597138ef88da954006ec1033
ZanderOl <olexandruk@gmail.com>
2023-03-21 00:00:13 +0200

    add search filter

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React from "react";
+import { Collection } from "./Collection";
 import "./index.scss";
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
-
 function App() {
+  const [categoryIs, setCategoryId] = React.useState(0);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [collections, setCollections] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://6418691c29e7e36438e7fcd9.mockapi.io/picture-library")
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("An error during getting responce from server");
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>My pictures collection</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
+          <li className="active">All</li>
           <li>Mountains</li>
           <li>Ocean</li>
           <li>Architecture</li>
           <li>Cities</li>
         </ul>
-        <input className="search-input" placeholder="Search by name" />
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Search by name"
+        />
       </div>
       <div className="content">
-        <Collection
-          name="World trips"
-          images={[
-            "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-          ]}
-        />
+        {collections
+          .filter((obj) => {
+            return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+          })
+          .map((obj, index) => (
+            <Collection key={index} name={obj.name} images={obj.photos} />
+          ))}
       </div>
       <ul className="pagination">
         <li>1</li>
