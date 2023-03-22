eb53f77107e6fe726b536d6de0eb02ee36f8a755
ZanderOl <olexandruk@gmail.com>
2023-03-22 15:15:45 +0200

    add pagination

--- src/App.js
+++ src/App.js
@@ -12,11 +12,19 @@ const cats = [
 
 function App() {
   const [categoryId, setCategoryId] = React.useState(0);
+  const [page, setPage] = React.useState(1);
+  const [isLoading, setIsLoading] = React.useState(true);
   const [searchValue, setSearchValue] = React.useState("");
   const [collections, setCollections] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("https://6418691c29e7e36438e7fcd9.mockapi.io/picture-library")
+    setIsLoading(true);
+
+    const category = categoryId ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://6418691c29e7e36438e7fcd9.mockapi.io/picture-library?page=${page}&limit=3&${category}`
+    )
       .then((res) => res.json())
       .then((json) => {
         setCollections(json);
@@ -24,8 +32,9 @@ function App() {
       .catch((err) => {
         console.warn(err);
         alert("An error during getting responce from server");
-      });
-  }, []);
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
 
   return (
     <div className="App">
@@ -51,18 +60,33 @@ function App() {
         />
       </div>
       <div className="content">
-        {collections
-          .filter((obj) => {
-            return obj.name.toLowerCase().includes(searchValue.toLowerCase());
-          })
-          .map((obj, index) => (
-            <Collection key={index} name={obj.name} images={obj.photos} />
-          ))}
+        {isLoading ? (
+          <h2>Loading ...</h2>
+        ) : (
+          collections
+            .filter((obj) => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} images={obj.photos} />
+            ))
+        )}
       </div>
       <ul className="pagination">
-        <li>1</li>
+        {[
+          [...Array(3)].map((_, i) => (
+            <li
+              key={i}
+              onClick={() => setPage(i + 1)}
+              className={page === i + 1 ? "active" : ""}
+            >
+              {i + 1}
+            </li>
+          )),
+        ]}
+        {/* <li>1</li>
         <li className="active">2</li>
-        <li>3</li>
+        <li>3</li> */}
       </ul>
     </div>
   );
